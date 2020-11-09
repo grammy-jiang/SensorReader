b1df6d8ab920cc9f27cc0a1d09c98bc2d2b6d095
Grammy Jiang <grammy.jiang@gmail.com>
2020-11-09 21:34:43 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,31 @@
 """
 setup.py for Sensor Reader
 """
-from setuptools import setup
+from setuptools import find_packages, setup
 
 import versioneer
 
-setup(version=versioneer.get_version(), cmdclass=versioneer.get_cmdclass())
+setup(
+    name="Sensor-Reader",
+    cmdclass=versioneer.get_cmdclass(),
+    version=versioneer.get_version(),
+    description="Read the sensors data and send to backends",
+    long_description="",
+    url="",
+    author="Grammy Jiang",
+    author_email="grammy.jiang@gmail.com",
+    classifiers=[],
+    keywords="Raspberry Pi",
+    project_urls={},
+    packages=find_packages(),
+    install_requires=[
+        "aiokafka",
+        "apscheduler",
+        "asyncpg",
+        "cachetools",
+        "sense-hat",
+        "uvloop",
+    ],
+    python_requires=">=3.8",
+    entry_points={"console_scripts": ["sensor_reader=sensor_reader.cli::main"]},
+)
