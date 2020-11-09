b9e621a024c735a853081ce97f1f9a56039b8e35
Grammy Jiang <grammy.jiang@gmail.com>
2020-11-10 08:27:56 +1100

    Add a mixin class for logger

---
+++ sensor_reader/utils/log.py
@@ -0,0 +1,27 @@
+"""
+Logging related utilities
+"""
+
+import logging
+from functools import cached_property
+
+
+class LoggerMixin:  # pylint: disable=too-few-public-methods
+    """
+    Logger Mixin
+    """
+
+    @cached_property
+    def logger(self) -> logging.Logger:
+        """
+
+        :return:
+        :rtype: logging.Logger
+        """
+        name = ".".join(
+            [
+                self.__module__,
+                self.__class__.___name__,
+            ]
+        )
+        return logging.getLogger(name)
