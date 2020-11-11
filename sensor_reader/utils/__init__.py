0e4d9149aca91841f1b53c188d85634038b47ca6
Grammy Jiang <grammy.jiang@gmail.com>
2020-11-11 11:37:04 +1100

    Add get_runtime_info into sensor_reader.utils.__init__

--- sensor_reader/utils/__init__.py
+++ sensor_reader/utils/__init__.py
@@ -1,11 +1,12 @@
 """
 All objects in sensor_reader.utils
 """
-from sensor_reader.utils.log import LoggerMixin, configure_logging
+from sensor_reader.utils.log import LoggerMixin, configure_logging, get_runtime_info
 from sensor_reader.utils.misc import load_object
 
 __all__ = [
     "configure_logging",
+    "get_runtime_info",
     "load_object",
     "LoggerMixin",
 ]
