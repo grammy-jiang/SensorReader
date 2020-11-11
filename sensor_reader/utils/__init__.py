0c2d8a722c6606bfaa3017c90ca9c7eab2e54443
Grammy Jiang <grammy.jiang@gmail.com>
2020-11-11 11:09:47 +1100

    Add configure_logging into sensor_reader.utils.__init__

--- sensor_reader/utils/__init__.py
+++ sensor_reader/utils/__init__.py
@@ -1,10 +1,11 @@
 """
 All objects in sensor_reader.utils
 """
-from sensor_reader.utils.log import LoggerMixin
+from sensor_reader.utils.log import LoggerMixin, configure_logging
 from sensor_reader.utils.misc import load_object
 
 __all__ = [
+    "configure_logging",
     "load_object",
     "LoggerMixin",
 ]
