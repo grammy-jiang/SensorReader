f96d2741c6452662ed113fa9b2cc8454733386f3
Grammy Jiang <grammy.jiang@gmail.com>
2020-11-11 11:06:34 +1100

    Add configure_logging and its test case

--- sensor_reader/utils/log.py
+++ sensor_reader/utils/log.py
@@ -5,6 +5,8 @@ Logging related utilities
 import logging
 from functools import cached_property
 
+from sensor_reader.settings import Settings
+
 
 class LoggerMixin:  # pylint: disable=too-few-public-methods
     """
@@ -25,3 +27,25 @@ class LoggerMixin:  # pylint: disable=too-few-public-methods
             ]
         )
         return logging.getLogger(name)
+
+
+def configure_logging(settings: Settings) -> None:
+    """
+
+    :param settings:
+    :type settings: Settings
+    :return:
+    :rtype: None
+    """
+    # Get a console handler and configure it
+    console_handler = logging.StreamHandler()
+
+    formatter = logging.Formatter(
+        fmt=settings["LOG_FORMATTER_FMT"],
+        datefmt=settings["LOG_FORMATTER_DATEFMT"],
+    )
+    console_handler.setFormatter(formatter)
+    console_handler.setLevel(settings["LOG_LEVEL"])
+
+    # add this console handler into logging
+    logging.root.addHandler(console_handler)
--- tests/utils/test_log.py
+++ tests/utils/test_log.py
@@ -1,10 +1,13 @@
 """
 The test cases for sensor_reader.utils.log
 """
+import logging
 from typing import List, Tuple
 from unittest import TestCase
+from unittest.mock import MagicMock, patch
 
-from sensor_reader.utils.log import LoggerMixin
+from sensor_reader.settings import Settings
+from sensor_reader.utils.log import LoggerMixin, configure_logging
 
 
 class TestClass(LoggerMixin):  # pylint: disable=too-few-public-methods
@@ -59,3 +62,38 @@ class LoggerMixinTest(TestCase):
             self.assertEqual(log_record.levelname, level.upper())
             self.assertEqual(log_record.msg, message)
             self.assertEqual(log_record.args, args)
+
+
+class FunctionsTest(TestCase):
+    """
+    the test cases for functions
+    """
+
+    @patch("logging.root")
+    def test_configure_logging(self, root: MagicMock):
+        """
+
+        :param root:
+        :type root: MagicMock
+        :return:
+        """
+        settings = Settings(
+            {
+                "LOG_LEVEL": logging.INFO,
+                "LOG_FORMATTER_FMT": "%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+                "LOG_FORMATTER_DATEFMT": "%Y-%m-%d %H:%M:%S",
+            }
+        )
+        configure_logging(settings)
+
+        root.addHandler.assert_called()
+
+        call_args = root.addHandler.call_args_list
+        handler: logging.StreamHandler = call_args[0].args[0]
+
+        self.assertEqual(handler.level, logging.INFO)
+        self.assertEqual(handler.formatter.datefmt, "%Y-%m-%d %H:%M:%S")  # type: ignore
+        self.assertEqual(
+            handler.formatter._fmt,  # pylint: disable=protected-access
+            "%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+        )
