b396c6c616b1a9c246fdb3de51a34845e74777a6
Grammy Jiang <grammy.jiang@gmail.com>
2020-11-14 09:11:05 +1100

    Update signals

--- sensor_reader/signals/__init__.py
+++ sensor_reader/signals/__init__.py
@@ -3,11 +3,42 @@ The signals used in Sensor Reader
 """
 from sensor_reader.signals.manager import SignalManager
 
-service_start = object()
-service_stop = object()
+
+class Signal:
+    def __init__(self, name: str):
+        """
+
+        :param name:
+        :type name: str
+        """
+        self.name = name
+
+    def __str__(self):
+        """
+
+        :return:
+        """
+        return self.name
+
+    def __repr__(self):
+        """
+
+        :return:
+        """
+        return self.name
+
+
+components_start = Signal("component_start")
+components_stop = Signal("component_stop")
+
+service_start = Signal("service_start")
+service_stop = Signal("service_stop")
 
 __all__ = [
+    "Signal",
     "SignalManager",
+    "components_start",
+    "components_stop",
     "service_start",
     "service_stop",
 ]
