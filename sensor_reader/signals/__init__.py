343e5d7cde721506cc1be805fb984f6b41156b71
Grammy Jiang <grammy.jiang@gmail.com>
2020-11-13 21:29:18 +1100

    Add two signals: service_start and service_stop

--- sensor_reader/signals/__init__.py
+++ sensor_reader/signals/__init__.py
@@ -3,6 +3,11 @@ The signals used in Sensor Reader
 """
 from sensor_reader.signals.manager import SignalManager
 
+service_start = object()
+service_stop = object()
+
 __all__ = [
     "SignalManager",
+    "service_start",
+    "service_stop",
 ]
