572999e40f8f431c9fd69a4f4fb388623e942bad
Grammy Jiang <grammy.jiang@gmail.com>
2020-11-16 08:30:34 +1100

    Update SensorHATReader

--- sensor_reader/readers/sensor_hat.py
+++ sensor_reader/readers/sensor_hat.py
@@ -4,6 +4,7 @@ Sensor HAT Reader
 from __future__ import annotations
 
 import asyncio
+from datetime import datetime, timedelta, timezone
 from typing import Dict, Tuple
 
 from sensor_reader.base import BaseComponent
@@ -16,6 +17,20 @@ class SensorHATReader(BaseComponent):
 
     name = "SensorHATReader"
     setting_prefix = "SENSOR_HAT_READER_"
+
+    def __init__(self, service, name: str = None, setting_prefix: str = None):
+        """
+
+        :param service:
+        :type service:
+        :param name:
+        :type name: str
+        :param setting_prefix:
+        :type setting_prefix: str
+        """
+        super().__init__(service, name, setting_prefix)
+
+        self.timezone = datetime.now(timezone(timedelta(0))).astimezone().tzinfo
 
     @classmethod
     def from_service(
@@ -48,30 +63,42 @@ class SensorHATReader(BaseComponent):
             self.get_temperature_from_pressure(),
         )
 
-    async def get_humidity(self) -> Dict[str, float]:
+    async def get_humidity(self) -> Dict:
         """
 
         :return:
-        :rtype: Dict[str, float]
+        :rtype: Dict
         """
+        return {
+            "timestamp": datetime.now().astimezone(self.timezone),
+        }
 
-    async def get_temperature_from_humidity(self) -> Dict[str, float]:
-        """
-
-        :return:
-        :rtype: Dict[str, float]
-        """
-
-    async def get_pressure(self) -> Dict[str, float]:
+    async def get_temperature_from_humidity(self) -> Dict:
         """
 
         :return:
-        :rtype: Dict[str, float]
+        :rtype: Dict
         """
+        return {
+            "timestamp": datetime.now().astimezone(self.timezone),
+        }
 
-    async def get_temperature_from_pressure(self) -> Dict[str, float]:
+    async def get_pressure(self) -> Dict:
         """
 
         :return:
-        :rtype: Dict[str, float]
+        :rtype: Dict
         """
+        return {
+            "timestamp": datetime.now().astimezone(self.timezone),
+        }
+
+    async def get_temperature_from_pressure(self) -> Dict:
+        """
+
+        :return:
+        :rtype: Dict
+        """
+        return {
+            "timestamp": datetime.now().astimezone(self.timezone),
+        }
